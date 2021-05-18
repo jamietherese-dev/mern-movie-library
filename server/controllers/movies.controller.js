34c1c2780c7f03e35c4f46b5955faa991b513df9
Maricar Albarando <albarma@macc02f321wmd6p.americas.manulife.net>
2021-05-18 13:28:05 +0800

    Refactor movies route

--- server/controllers/movies.controller.js
+++ server/controllers/movies.controller.js
@@ -1,10 +1,32 @@
-import Movie from '../models/movie.model.js';
+import Movie from "../models/movie.model.js";
 
-export const getMovies = async (req, res) => { 
+export const getMovies = async (req, res) => {
   try {
-    const movies = await Movie.find().sort({createdAt: 'desc'});
+    const movies = await Movie.find().sort({ createdAt: "desc" });
     res.status(200).json(movies);
   } catch (error) {
-    res.status(404).json({message: error.message });
+    res.status(404).json({ message: error.message });
   }
-}
+};
+
+export const createMovie = async (req, res) => {
+  const movie = req.body;
+  console.log("movie", movie);
+  const newMovie = new Movie(movie);
+  try {
+    await newMovie.save();
+    res.status(201).json(newMovie);
+  } catch (error) {
+    res.status(409).json({ message: error.message });
+  }
+};
+
+export const deleteMovie = async (req, res) => {
+  console.log("req.params", req.params);
+  try {
+    const movie = await Movie.findByIdAndDelete(req.params.id);
+    res.status(200).json("Movie deleted.");
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+};
